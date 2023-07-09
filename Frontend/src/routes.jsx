8668bc7e8a11fcd5ab46e2db80d1a0d8d0bec914
= <plaeis260@gmail.com>
2023-07-09 11:51:33 -0500

    actualizacion

--- Frontend/src/routes.jsx
+++ Frontend/src/routes.jsx
@@ -15,7 +15,7 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-
+    
 import Icons from "./views/Icons";
 import Machinery from "./views/Machinery";
 import User from "./views/User";
